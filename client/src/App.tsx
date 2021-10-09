62204668366f8b2be8da65e3650d71857ee1c2a7
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-10-09 13:20:19 +0100

    interval clearing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,29 @@
 import React, { FC, useEffect, useState } from "react";
-import { Button, VStack } from "@chakra-ui/react";
+import { Button, Switch, VStack } from "@chakra-ui/react";
 import { Coord, deleteCoords, getCoords } from "./api/api";
 import CoordTable from "./components/Table";
 
 const App: FC = () => {
   const [coords, setCoords] = useState<Coord[] | []>([]);
+  const [toggleFetch, setToggleFetch] = useState<boolean>(false);
 
   useEffect(() => {
-    setInterval(() => {
-      getCoords().then((res) => setCoords(res));
-    }, 100);
-  }, []);
+    if (toggleFetch) {
+      setInterval(() => {
+        getCoords().then((res) => setCoords(res));
+      }, 100);
+    } else {
+      // janky way to clear all intervals
+      for (let i = 0; i < 100; i++) {
+        window.clearInterval(i);
+      }
+    }
+  }, [toggleFetch]);
 
   return (
     <div>
       <VStack justifyContent="center">
+        <Switch onChange={() => setToggleFetch(!toggleFetch)}></Switch>
         <Button onClick={() => deleteCoords()}>Delete</Button>
         <CoordTable coordList={coords}></CoordTable>
       </VStack>
